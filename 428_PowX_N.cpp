ca68a144878b5c1b905446fbf72ba7b49f40f73e
feary <fearycxy@gmail.com>
2018-06-12 21:13:53 +0800

    0612

--- 428_PowX_N.cpp
+++ 428_PowX_N.cpp
@@ -3,11 +3,12 @@ public:
     
      double myPow(double x, int n) {
         // write your code here
+        cout << x <<", "<<n<<endl;
         if(n==0) return 1;
         double an = myPow(x,n/2);
         an *=an;
         if(n%2==0) return an;
-        return an * n>0?x:1/x;
+        return an * (n>0?x:1/x);
     }
 
 };
